f44b4bb964245bfe0ec104ad4688731bb282fb51
Akshay <akshay.padalkar@iaai.com>
2021-12-18 15:22:09 +0530

    Remove duplicate project

--- DemoApplication/Akshay_task/app/src/main/java/com/example/shaadicom/APIService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shaadicom
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface APIService {
-    @GET("api/?results=10")
-    suspend fun fetchUserData(): Response<UserModel>
-}
--- DemoApplication/Akshay_task/app/src/main/java/com/example/shaadicom/adapter/RecyclerViewOnItemClickListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.shaadicom.adapter
-
-
-interface RecyclerViewOnItemClickListener {
-    fun onItemClick(id: Int, viewmodel: UserMatchCardViewModel,adapterPosition:Int)
-}
